d1c60915bd9559d8fa34a3498c086800fd22e70e
asobacorp-2 <shingi@AsobaCorp2s-MacBook-Air.local>
2026-03-15 17:59:37 -0500

    Bump package version to 0.5.0

--- src/python/odse/__init__.py
+++ src/python/odse/__init__.py
@@ -5,7 +5,7 @@ A Python library for validating and transforming energy asset data
 using the ODS-E specification.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from .validator import (
     validate,
