bcc542f2859a43872cd896cecc8150df6bb31b72
Shingi Samudzi <samudzi@gmail.com>
2026-05-01 10:51:25 -0500

    Release version 0.6.0
    - Update CHANGELOG.md with Sungrow iSolarCloud API additions
    - Bump version to 0.6.0 in __init__.py and pyproject.toml
    - Add comprehensive release notes for Sungrow transformer and specification

--- src/python/odse/__init__.py
+++ src/python/odse/__init__.py
@@ -5,7 +5,7 @@ A Python library for validating and transforming energy asset data
 using the ODS-E specification.
 """
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 from .validator import (
     validate,
